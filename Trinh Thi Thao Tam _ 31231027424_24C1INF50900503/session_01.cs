fca9b01615025f42a44c2c45d7136eb9295751fa
31231027424@ueh.edu.vn <31231027424@ueh.edu.vn>
2024-10-23 18:50:12 +0700

    Add project files.

---
+++ Trinh Thi Thao Tam _ 31231027424_24C1INF50900503/session_01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trinh_Thi_Thao_Tam___31231027424_24C1INF50900503
+{
+    internal class session_01
+    {
+        static void Main()
+        {
+            Question_01();
+            Console.ReadKey();
+        }
+        public static void Question_01()
+        {
+            Console.WriteLine("hello, world");
+        }
+    }
+}
---
+++ Trinh Thi Thao Tam _ 31231027424_24C1INF50900503/session_02.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trinh_Thi_Thao_Tam___31231027424_24C1INF50900503
+{
+    internal class session_02
+    {
+        public static void Main()
+        {
+            //Question_01();
+            //Question_02();
+            //Question_03();
+            //Question_04();
+            //Question_05();
+            //Question_06();
+            //Question_07();
+            //Question_08();
+            //Question_09();
+            Question_10();
+            Console.ReadKey();
+            // Cau 1: Add/Sum Two Numbers
+        }
+        public static void Question_01()
+        {
+            Console.Write("Nhap so a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so b:");
+            int b = int.Parse(Console.ReadLine());
+            int c = a + b;
+            Console.Write($"{a}+{b} = {c}");
+        }
+        // Cau 2: Swap values of two variables
+        public static void Question_02()
+        {
+            Console.Write("Nhap so a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so b:");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write($"Truoc khi hoan doi a = {a}, b = {b}");
+            int c = a;
+            a = b;
+            b = c;
+            Console.Write($"Sau khi hoan doi a = {a}, b = {b}");
+        }
+        // Cau 3: Multiply two floating point numbers
+        public static void Question_03()
+        {
+            Console.Write("Nhap so a:");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("Nhap so b:");
+            float b = float.Parse(Console.ReadLine());
+            float c = a * b;
+            Console.Write($"{a} * {b} = {c}");
+        }
+        // Cau 4: Convert feet to meter
+        public static void Question_04()
+        {
+            Console.Write($"Nhap so feet:");
+            float f = float.Parse(Console.ReadLine());
+            double m = f * 0.3048;
+            Console.Write($"f * 0.3048 = {m}");
+        }
+        // Cau 5: Conver Celsius to Fahrenheit and vice versa
+        public static void Question_05()
+        {
+            Console.Write("Nhap so Celsius:");
+            float c = float.Parse(Console.ReadLine());
+            double f = c * 33.8;
+            Console.Write($"c * 33.8 = {f}");
+        }
+        // Cau 6: Find the size of data type
+        public static void Question_06()
+        {
+            int s = sizeof(int);
+            Console.WriteLine($"(s)");
+        }
+        // Cau 7: Print ASCII Value
+        public static void Question_07()
+        {
+            char c = 'a';
+            int num = c;
+            Console.WriteLine($"{c} in ASCII = {num}");
+        }
+        // Cau 8: Calculate the area of circle
+        public static void Question_08()
+        {
+            Console.Write("Nhap ban kinh:");
+            float r = float.Parse(Console.ReadLine());
+            double area = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine($"Math.PI * Math.Pow(r,2) = {area}");
+        }
+        // Cau 9: Calculate the area of square
+        public static void Question_09()
+        {
+            Console.Write("Nhap do dai canh:");
+            float a = float.Parse(Console.ReadLine());
+            double area = Math.Pow(a, 2);
+            Console.Write($"Math.Pow(a,2) = {area}");
+        }
+        // Cau 10: Convert days to years, weeks and days
+        public static void Question_10()
+        {
+            Console.Write("Nhap so ngay:");
+            int days = int.Parse(Console.ReadLine());
+            int year = days / 365;
+            int week = (days - year * 365) / 7;
+            int day = (days - year % 7) / 7;
+            Console.Write($"{days} days = {year} years, {week} weeks, {day} days");
+        }
+    }
+}
