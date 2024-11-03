69068e7637cf74f29fb68130371433b2a2ad911b
31231027424@ueh.edu.vn <31231027424@ueh.edu.vn>
2024-11-03 21:40:10 +0700

    update 3/11

--- Trinh Thi Thao Tam _ 31231027424_24C1INF50900503/session_05_1.cs
+++ Trinh Thi Thao Tam _ 31231027424_24C1INF50900503/session_05_1.cs
@@ -21,16 +21,44 @@ namespace Trinh_Thi_Thao_Tam___31231027424_24C1INF50900503
                     return false;
             return true;
         }
-        static void printPrimenumberUnderN(int n)
+        static void printPrimenumberunderN(int n)
         {
             for (int i = 2; i <= n; i++)
                 if (isPrime(i))
                     Console.WriteLine(i);
         }
+        static void printfirstNprime(int n)
+        {
+            int so = 2;
+            int dem = 0;
+            while (dem < n)
+            {
+                if (isPrime(so))
+                {
+                    Console.Write(so + " ");
+                    dem++;
+                }
+                so++;
+            }
+        }
+        //Cau2: Write a C# function to calculate the factorial of a number (a non-negative integer). The function accepts the number as an argument.
+        static int theFactorialofNumber (int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result = result * i; //hoac co the la result *= i;
+            }
+            return result;
+        }
         public static void Main()
         {
-            int m = maxofthreenumbers(7, 8, 9);
-            Console.WriteLine(m);
+            //int m = maxofthreenumbers(7, 8, 9);
+            //Console.WriteLine(m);
+            Console.Write("Nhap mot so n:");
+            int n = int.Parse(Console.ReadLine());
+            int result = theFactorialofNumber(n);
+            Console.WriteLine($"Giai thua cua {n} la {result}");
             Console.ReadKey();
         }
     }
