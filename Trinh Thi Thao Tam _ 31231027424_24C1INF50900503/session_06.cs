0f547b52672dab03b1592a538979fb7850f975c7
31231027424@ueh.edu.vn <31231027424@ueh.edu.vn>
2024-11-09 21:15:45 +0700

    update 9/11

--- Trinh Thi Thao Tam _ 31231027424_24C1INF50900503/session_06.cs
+++ Trinh Thi Thao Tam _ 31231027424_24C1INF50900503/session_06.cs
@@ -13,34 +13,43 @@ namespace Trinh_Thi_Thao_Tam___31231027424_24C1INF50900503
             //Enter item values
             Console.Write("Nhap N:");
             int N = int.Parse(Console.ReadLine());
-            int[] a;
-            a = new int[N];
+            int[] a = new int[N];
             nhap(a);
+            inmang(a);
+            tang2(a);
+            Console.ReadKey();
         }
         static void nhap(int[] a)
         {
             for (int i = 0; i < a.Length; i++)
             {
-                Console.WriteLine($"Nhap phan tu thu {i + 1}:");
+                Console.Write($"Nhap phan tu thu {i + 1}:");
                 a[i] = int.Parse(Console.ReadLine());
             }
+        }
             //Print the array to screen
-            static void InMang(int[] a)
-            {
-                for (int i = 0; i < a.Length; i++)
+        static void inmang(int[] a) 
+        {
+             Console.Write($"Day array la: ");
+             for (int i = 0; i < a.Length; i++)
                 {
-                    Console.WriteLine($"Day array la: {a[i]}");
+                    Console.Write(a[i] + " ");
                 }
-                static void IncreaseEachItem(int[] a)
+            Console.WriteLine();
+        }
+        static void tang2(int[] a)
             //Adding it with 2
-            {
-                for (int i = 0; i < a.Length; i++)
-                    {
+        {
+             for (int i = 0; i < a.Length; i++)
+                 {
                         a[i] += 2;
                         Console.WriteLine($"Gia tri item sau khi cong 2 la: {a[i]}");
-                    }
-            }
-            }
+                  }
         }
     }
 }
+
+
+
+
+
